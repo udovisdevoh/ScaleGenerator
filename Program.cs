523ffb9cd1c38d097427a010a16ff67a47746d9c
Guillaume Lacasse <glacasse@gmail.com>
2019-04-13 15:19:26 -0400

    It works

--- Program.cs
+++ Program.cs
@@ -12,17 +12,21 @@ namespace ScaleGenerator
         {
             ScaleGenerator scaleGenerator = new ScaleGenerator();
 
-            const int noteCountInScale = 5;
+            const int noteCountInScale = 7;
             const bool isEnableMultipleSameSizeChordOnSameRootNote = false;
 
             Chord minor = new Chord(0, 3, 7);
             Chord major = new Chord(0, 4, 7);
             Chord augmented = new Chord(0, 4, 8);
             Chord diminished = new Chord(0, 3, 6);
+            Chord powerChord = new Chord(0, 7);
+            Chord fourth = new Chord(0, 5);
 
             //scaleGenerator.AddChord(minor, 2);
-            scaleGenerator.AddChord(major, 2);
-            //scaleGenerator.AddChord(diminished, 2);
+            //scaleGenerator.AddChord(major, 1);
+            scaleGenerator.AddChord(powerChord, 6);
+            //scaleGenerator.AddChord(fourth, 5);
+            //scaleGenerator.AddChord(diminished, 1);
             //scaleGenerator.AddChord(augmented, 1);
 
             IEnumerable<Chord> scales = scaleGenerator.GenerateScales(noteCountInScale, isEnableMultipleSameSizeChordOnSameRootNote);
